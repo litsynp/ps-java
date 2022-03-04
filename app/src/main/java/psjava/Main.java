40e29ad1fa19cbd5f0dbba83717b740b9690ccba
litsynp <nocte55is@gmail.com>
2022-03-04 22:53:55 +0900

    build: add template

---
+++ app/src/main/java/psjava/Main.java
@@ -0,0 +1,12 @@
+package psjava;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+  }
+}
---
+++ app/src/test/java/psjava/AppTest.java
@@ -0,0 +1,4 @@
+package psjava;
+
+class MainTest {
+}
