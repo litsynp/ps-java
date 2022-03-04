791dbff1f5581fd71e797d225487d8239b36b676
litsynp <nocte55is@gmail.com>
2022-03-05 03:21:13 +0900

    solve: add BJ Q10926

---
+++ app/src/main/java/psjava/baekjoon/Q10926.java
@@ -0,0 +1,14 @@
+package psjava.baekjoon;
+
+import java.io.*;
+
+/**
+ * 백준 10926 : ??! : 2022.03.04
+ */
+public class Q10926 {
+  public static void main(String[] args) throws IOException {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      System.out.println(br.readLine() + "??!");
+    }
+  }
+}
