b6bb9d7507a55aad7c36ec688bed5ba4a6dbfcb5
litsynp <nocte55is@gmail.com>
2022-03-05 03:21:43 +0900

    solve: add BJ Q2480

---
+++ app/src/main/java/psjava/baekjoon/Q2480.java
@@ -0,0 +1,34 @@
+package psjava.baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 2022.03.04 : 백준 2480 : 주사위 세개
+ */
+public class Q2480 {
+
+  public static void main(String[] args) throws IOException {
+
+    StringTokenizer st;
+
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      st = new StringTokenizer(br.readLine(), " ");
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int c = Integer.parseInt(st.nextToken());
+
+      int score = 0;
+      if (a == b && b == c) {
+        score = 10_000 + a * 1_000;
+      } else if (a == b || a == c) {
+        score = 1_000 + a * 100;
+      } else if (b == c) {
+        score = 1_000 + b * 100;
+      } else {
+        score = Math.max(Math.max(a, b), c) * 100;
+      }
+      System.out.println(score);
+    }
+  }
+}
