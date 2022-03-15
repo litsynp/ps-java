2a7e8f5440b3b54cfd72d0a5f978d0f2c6754931
litsynp <nocte55is@gmail.com>
2022-03-15 21:38:32 +0900

    feat: update template

--- app/src/main/java/psjava/Main.java
+++ app/src/main/java/psjava/Main.java
@@ -1,12 +1,35 @@
 package psjava;
 
 import java.io.*;
-import java.lang.*;
 import java.util.*;
 
+/**
+ * yyyy.MM.dd - Site - #NNNN Title
+ */
 public class Main {
 
   public static void main(String[] args) throws IOException {
+    ps.init();
 
+    ps.close();
+  }
+
+  static class ps {
+
+    public static BufferedReader br;
+    public static BufferedWriter bw;
+    public static StringBuilder sb;
+
+    public static void init() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+      bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      sb = new StringBuilder();
+    }
+
+    public static void close() throws IOException {
+      bw.write(sb.toString());
+      br.close();
+      bw.close();
+    }
   }
 }
