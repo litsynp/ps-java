6c6e7b0809a3d2d960cbc7bac22479323c19bb0d
litsynp <nocte55is@gmail.com>
2022-03-19 16:26:42 +0900

    feat: add getIntInputs() to ps

--- app/src/main/java/psjava/util/ps.java
+++ app/src/main/java/psjava/util/ps.java
@@ -1,6 +1,7 @@
 package psjava.util;
 
 import java.io.*;
+import java.util.*;
 
 /**
  * 문제풀이에서 I/O를 담당하는 클래스
@@ -21,5 +22,9 @@ public class ps {
     bw.write(sb.toString());
     br.close();
     bw.close();
+  }
+
+  public static int[] getIntInputs() throws IOException {
+    return Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
   }
 }
