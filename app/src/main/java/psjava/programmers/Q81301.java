7c43d08d22819a7a830132293135d6f18c593a7d
litsynp <nocte55is@gmail.com>
2022-03-22 01:55:07 +0900

    solve: add programmers Q81301

---
+++ app/src/main/java/psjava/programmers/Q81301.java
@@ -0,0 +1,18 @@
+package psjava.programmers;
+
+/**
+ * 2022.03.22 - 프로그래머스 - #81301 숫자 문자열과 영단어
+ */
+public class Q81301 {
+
+    public static int solution(String s) {
+        String[] num = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        String[] word = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        for (int i = 0; i < 10; i++) {
+            s = s.replace(word[i], num[i]);
+        }
+
+        return Integer.parseInt(s);
+    }
+}
