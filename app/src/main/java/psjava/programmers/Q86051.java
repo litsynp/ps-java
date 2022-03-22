5a2060c77e0441d4aba260d4c8cf398189897fe5
litsynp <nocte55is@gmail.com>
2022-03-23 00:10:44 +0900

    solve: add programmers Q86051

---
+++ app/src/main/java/psjava/programmers/Q86051.java
@@ -0,0 +1,17 @@
+package psjava.programmers;
+
+/**
+ * 2022.03.23 - 프로그래머스 - #86051 없는 숫자 더하기
+ */
+public class Q86051 {
+
+    public int solution(int[] numbers) {
+        int answer = 45;
+
+        for (int n : numbers) {
+            answer -= n;
+        }
+
+        return answer;
+    }
+}
