1e6c276839ec0f550aa4ccb937a3d5c8af09074e
litsynp <nocte55is@gmail.com>
2022-03-23 01:20:00 +0900

    solve: add programmers Q70128

---
+++ app/src/main/java/psjava/programmers/Q70128.java
@@ -0,0 +1,15 @@
+package psjava.programmers;
+
+/**
+ * 2022.03.23 - 프로그래머스 - #70128 내적
+ */
+public class Q70128 {
+
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
