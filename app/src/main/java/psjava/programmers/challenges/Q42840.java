26e2c6de44d1d4b0111d461440af1102fbd41d5f
litsynp <nocte55is@gmail.com>
2022-04-20 01:16:41 +0900

    solve: add programmers Q42840

---
+++ app/src/main/java/psjava/programmers/challenges/Q42840.java
@@ -0,0 +1,36 @@
+package psjava.programmers.challenges;
+
+import java.util.*;
+
+/**
+ * 2022.04.20 - 프로그래머스 - #42840 모의고사
+ */
+public class Q42840 {
+
+    public int[] solution(int[] answers) {
+        int[][] patterns = {
+                { 1, 2, 3, 4, 5 },
+                { 2, 1, 2, 3, 2, 4, 2, 5 },
+                { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 }
+        };
+
+        int[] counts = new int[3];
+        for (int i = 0; i < answers.length; i++) {
+            for (int n = 0; n < 3; n++) {
+                if (answers[i] == patterns[n][i % patterns[n].length]) {
+                    counts[n] += 1;
+                }
+            }
+        }
+
+        int max = Math.max(counts[0], Math.max(counts[1], counts[2]));
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] == max) {
+                answer.add(i + 1);
+            }
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
