49d3697582cf088ceb39fed788fcbbf1ba4fd057
litsynp <nocte55is@gmail.com>
2022-05-15 14:48:16 +0900

    solve: add programmers Q59046

---
+++ app/src/main/java/psjava/programmers/challenges/level2/Q59046.sql
@@ -0,0 +1,16 @@
+-- 2022.05.15 - 프로그래머스 - #59041 루시와 엘라 찾기
+SELECT
+  ANIMAL_ID,
+  NAME,
+  SEX_UPON_INTAKE
+FROM
+  ANIMAL_INS
+WHERE
+  NAME IN (
+    'Lucy',
+    'Ella',
+    'Pickle',
+    'Rogan',
+    'Sabrina',
+    'Mitty'
+  );
