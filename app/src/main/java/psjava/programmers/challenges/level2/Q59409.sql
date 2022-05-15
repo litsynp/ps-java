fc8e1cf80f81770607d7f9a55d39d775c0738986
litsynp <nocte55is@gmail.com>
2022-05-15 17:58:13 +0900

    solve: add programmers Q59409

---
+++ app/src/main/java/psjava/programmers/challenges/level2/Q59409.sql
@@ -0,0 +1,12 @@
+-- 2022.05.15 - 프로그래머스 - #59409 중성화 여부 파악하기
+SELECT
+  ANIMAL_ID,
+  NAME,
+  CASE
+    WHEN SEX_UPON_INTAKE LIKE 'Intact%' THEN 'X'
+    ELSE 'O'
+  END AS "중성화"
+FROM
+  ANIMAL_INS
+ORDER BY
+  ANIMAL_ID;
