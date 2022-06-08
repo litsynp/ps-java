30f36e750816977239c65811c967867eb3882617
litsynp <nocte55is@gmail.com>
2022-06-09 00:30:55 +0900

    fix: fix programmers Q72414

--- app/src/main/java/psjava/programmers/challenges/level3/Q72415.java
+++ app/src/main/java/psjava/programmers/challenges/level3/Q72415.java
@@ -9,26 +9,27 @@ import java.util.*;
  * <p>
  * 2021 KAKAO BLIND RECRUITMENT
  *
+ * @see <a href="https://loosie.tistory.com/170">https://loosie.tistory.com/170</a>
  * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/72415">https://programmers.co.kr/learn/courses/30/lessons/72415</a>
  */
 public class Q72415 {
 
-    int answer;
-    final int[] dx = {-1, 1, 0, 0};
-    final int[] dy = {0, 0, -1, 1};
-
-    int[] cards;
-    int cardNum;
+    final int[] dx = {1, -1, 0, 0};
+    final int[] dy = {0, 0, 1, -1};
     List<String> orders;
 
     public int solution(int[][] board, int r, int c) {
-        initCard(board);
+        // 남아있는 카드 짝 개수 구하기
+        int cardNum = initCardNum(board);
+        int[] cards = initCards(cardNum);
+
+//        System.out.println(Arrays.toString(cards));
 
         // 카드의 순서 순열 생성 - orders
         orders = new ArrayList<>();
         permutation("", 0, cards);
 
-        answer = Integer.MAX_VALUE;
+        int answer = Integer.MAX_VALUE;
 
         for (String order : orders) {
             // 카드의 순서
@@ -46,14 +47,14 @@ public class Q72415 {
                 int targetCard = Integer.parseInt(targetCardStr);
 
                 // Find first card
-                totalMove += bfs(copyBoard, pos, cardNum);
+                totalMove += bfs(copyBoard, pos, targetCard);
                 copyBoard[pos[0]][pos[1]] = 0;
 
                 // Enter
                 totalMove++;
 
                 // Find second card
-                totalMove += bfs(copyBoard, pos, cardNum);
+                totalMove += bfs(copyBoard, pos, targetCard);
                 copyBoard[pos[0]][pos[1]] = 0;
 
                 // Enter
@@ -66,8 +67,29 @@ public class Q72415 {
         return answer;
     }
 
+    private int[] initCards(int cardNum) {
+        int[] cards = new int[cardNum];
+        for (int i = 0; i < cardNum; i++) {
+            cards[i] = i + 1;
+        }
+        return cards;
+    }
+
+    private int initCardNum(int[][] board) {
+        int cardNum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (board[i][j] != 0) {
+                    cardNum++;
+                }
+            }
+        }
+        cardNum /= 2;
+        return cardNum;
+    }
+
     // Search card
-    private int bfs(int[][] board, int[] pos, int targetCard) {
+    int bfs(int[][] board, int[] pos, int targetCard) {
         Queue<Node> q = new LinkedList<>();
         boolean[][] check = new boolean[4][4];
 
@@ -84,7 +106,7 @@ public class Q72415 {
 
             if (board[px][py] == targetCard) {
                 // Found the target at (px, py)
-                System.out.println("Target " + targetCard + "found at (" + px + ", " + py + ")");
+//                System.out.println("Target " + targetCard + " found at (" + px + ", " + py + ")");
                 pos[0] = px;
                 pos[1] = py;
                 return move;
@@ -92,8 +114,8 @@ public class Q72415 {
 
             // Move 1 block
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = px + dx[i];
+                int ny = py + dy[i];
 
                 if (isOut(nx, ny)) {
                     continue;
@@ -103,6 +125,7 @@ public class Q72415 {
                     continue;
                 }
 
+                check[nx][ny] = true;
                 q.add(new Node(nx, ny, move + 1));
             }
 
@@ -112,7 +135,7 @@ public class Q72415 {
                 int nx = res.x;
                 int ny = res.y;
 
-                if (isOut(nx, ny)) {
+                if (nx == x && ny == y) {
                     continue;
                 }
 
@@ -120,32 +143,12 @@ public class Q72415 {
                     continue;
                 }
 
+                check[nx][ny] = true;
                 q.add(new Node(nx, ny, move + 1));
             }
         }
 
         return 0;
-    }
-
-
-    private void initCard(int[][] board) {
-        // 남아있는 카드 짝 개수 구하기
-        cardNum = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (board[i][j] != 0) {
-                    cardNum++;
-                }
-            }
-        }
-        cardNum /= 2;
-
-        cards = new int[cardNum];
-        for (int i = 0; i < cardNum; i++) {
-            cards[i] = i + 1;
-        }
-
-        System.out.println(Arrays.toString(cards));
     }
 
     boolean isOut(int x, int y) {
@@ -155,26 +158,26 @@ public class Q72415 {
     /**
      * 뒤집을 카드의 순서를 모든 경우의 수로 orders에 담는다.
      */
-    private void permutation(String selected, int depth, int[] card) {
-        if (depth == card.length) {
+    void permutation(String selected, int depth, int[] cards) {
+        if (depth == cards.length) {
             orders.add(selected);
             return;
         }
 
-        for (int i = 0; i < card.length; i++) {
-            int num = card[i];
+        for (int i = 0; i < cards.length; i++) {
+            int num = cards[i];
             String numStr = String.valueOf(num);
 
             // 카드를 고른 적 없다면
             if (!selected.contains(numStr)) {
-                permutation(selected + num, depth + 1, card);
+                permutation(selected + num, depth + 1, cards);
             }
         }
     }
 
-    private Node checkRoute(int x, int y, int direction, int[][] board) {
+    Node checkRoute(int x, int y, int direction, int[][] board) {
         x += dx[direction];
-        y += dx[direction];
+        y += dy[direction];
 
         while (!isOut(x, y)) {
             if (board[x][y] != 0) {
@@ -182,9 +185,10 @@ public class Q72415 {
             }
 
             x += dx[direction];
-            y += dx[direction];
+            y += dy[direction];
         }
 
+        // 카드가 없으면 종료
         return new Node(x - dx[direction], y - dy[direction], 0);
     }
 
@@ -198,4 +202,3 @@ public class Q72415 {
         }
     }
 }
-
